c32a2fd5741d18aa6238453e898415bc7c86b79a
Andreas Rottmann <a.rottmann@gmx.at>
2009-11-01 12:22:14 +0100

    Updated `pkg-list.scm'

--- pkg-list.scm
+++ pkg-list.scm
@@ -1,8 +1,11 @@
 ;;@ A package manager for R6RS implementations.
 (package (dorodango)
-  (depends (spells))
+  (depends (srfi)
+           (spells)
+           (industria)
+           (ocelotl))
   (libraries (libs -> "dorodango"))
-  (programs (("scripts" "doro.sps") -> "doro.sps")))
+  (programs (("scripts" "doro.sps") -> "doro")))
 
 ;; Local Variables:
 ;; scheme-indent-styles: (pkg-list)

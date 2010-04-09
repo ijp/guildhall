8f75c35f9e6dbd2a96a63dffd77fe9ee1e52f1bf
Andreas Rottmann <a.rottmann@gmx.at>
2010-04-09 16:40:01 +0200

    pkg-list.scm: added installation hook installing the `doro.1' manpage

--- pkg-list.scm
+++ pkg-list.scm
@@ -7,7 +7,22 @@
            (ocelotl))
   (libraries (sls -> "dorodango")
              ("private" -> ("dorodango" "private")))
-  (programs (("scripts" "doro.sps") -> "doro")))
+  (programs (("scripts" "doro.sps") -> "doro"))
+  (installation-hook ()
+    (import (except (rnrs) file-exists? delete-file)
+            (srfi :8 receive)
+            (spells fmt)
+            (spells pathname)
+            (spells filesys)
+            (dorodango ui cmdline)
+            (dorodango ui cmdline help))
+    (lambda (agent)
+      (receive (pathname port) (create-temp-file)
+        (call-with-port port
+          (lambda (port)
+            (fmt port (dsp-man-page (command-list)))))
+        (agent 'install-file 'man "doro.1" (->namestring pathname))
+        (delete-file pathname)))))
 
 ;; Local Variables:
 ;; scheme-indent-styles: (pkg-list)

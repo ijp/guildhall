d664c8d2fa0531ee4e0b0bb44d4a3610e60e60df
Andy Wingo <wingo@pobox.com>
2011-07-21 10:52:29 +0200

    closer to distcheck
    * Makefile.am (NOCOMP_SOURCES): Install included files.
      (TESTDATA): Dist data used by the tests.
    
    * tests/tests.scm: Remove useless file.

--- tests/tests.scm
+++
@@ -1,9 +0,0 @@
-((files "utils.scm"
-        "inventory.scm"
-        "expression.scm"
-        "package.scm"
-        "bundle.scm"
-        "hooks.scm"
-        "database.scm"
-        "solver.scm"
-        "actions.scm"))
